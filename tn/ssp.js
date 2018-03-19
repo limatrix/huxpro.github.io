8dd26a3d9ad6c2e6e662b3f5200c48e1a381d248
shawnyin <shawnyin@yeah.net>
2018-03-19 19:56:18 +0800

    0

--- tn/ssp.js
+++ tn/ssp.js
@@ -67,6 +67,10 @@
             this.page_runtime_list.push(page_obj_top)
         }, 
 
+        length: function() {
+            return this.page_runtime_list.length;
+        },
+
         find: function(page_id) {
             for (var i = 0; i < this.page_template_list.length; i++) {
                 var page_obj = this.page_template_list[i];
@@ -123,3 +127,241 @@
             }
         },
     };
+
+//
+var Topic = function() {
+    this.cur_object_ptr  = null;
+    this.cur_object_deep = 0;
+    this.cur_object_seq  = 0;
+    this.global_object   = null;
+    this.ssp             = null;
+    this.start_x         = 0;
+    this.end_x           = 0;
+    this.save_tag        = 0;
+    this.cur_opt_item_id = "";
+    this.init();
+}
+
+Topic.prototype = {
+    init: function() {
+        this._init_global_object();
+        this._init_ssp();
+        this._init_index();
+        this._bind_edit_click();
+        this._bind_save_click();
+        this._bind_item_click();
+        this._bind_back_click();
+        this._bind_touch();
+        this._bind_oper_icon();
+    }, 
+
+    _init_global_object: function() {
+        this.global_object  = new Object();
+        this.global_object.id = 0;
+        this.global_object.text = "";
+        this.global_object.children = [];
+        // 从服务器拿来数据后需要再初始化children
+        this._set_cur_obj(this.global_object.children);
+    },
+
+    _init_ssp: function() {
+        this.ssp = new Ssp($(".main-container"), "topic");
+    },
+
+    _init_index: function() {
+        this.ssp.go("topic-list", this._get_page_deep(), this._get_cur_obj());
+    },
+
+    _bind_edit_click: function() {
+        var _this = this;
+        $(".topic-edit").click(function(){
+            _this.ssp.go("topic-edit")
+            _this._show_nav_bar_icon("topic-save");
+            _this.save_tag = 1;
+            console.log("_bind_edit_click");
+        });
+    },
+
+    _bind_save_click: function() {
+        var _this = this;
+        $(".topic-save").click(function(){
+            var text = $(".weui-textarea").val();
+            var cur_obj = _this._get_cur_obj();
+            if(_this.save_tag == 1) { //新增
+                if("" != text) {
+                    var obj = new Object();
+                    obj.id = _this._get_new_id();
+                    obj.text = text;
+                    obj.children = [];
+
+                    cur_obj.push(obj);
+                }
+            } else if(_this.save_tag == 2) { //修改
+                var obj = _this._get_obj_by_id(_this.cur_opt_item_id);
+                console.log(obj);
+                obj.text = text;
+                console.log(obj);
+            } else {
+                console.log("this.save_tag error : " + _this.save_tag);
+            }
+            _this.save_tag = 0;
+            _this.ssp.go("topic-list", _this._get_page_deep(), cur_obj);
+
+            _this._show_nav_bar_icon("topic-edit");
+        });
+    },
+
+    _bind_item_click: function() {
+        var _this = this;
+        $(document).on("click", ".topic-item", function(){
+            console.log(this)
+            var id = $(this).attr("id");
+            var obj = _this._get_obj_by_id(id);
+            if(obj) {
+                _this._incr_page_deep();
+                _this._set_cur_obj(obj.children);
+                _this.ssp.go("topic-list", _this._get_page_deep(), _this._get_cur_obj());
+            } else {
+                console.log("index page, get topic failed")
+            }
+        });
+    },
+
+    _bind_back_click: function() {
+        var _this = this;
+        $(".history-back").click(function(){
+            console.log("ssp back");
+            var obj = _this.ssp.back();
+            console.log(obj);
+            if(obj) {
+                _this._set_page_deep(obj.deep);
+                _this._set_cur_obj(obj.pointer);
+            }
+            
+        });
+    },
+
+    _bind_touch: function() {
+        var _this = this;
+        $(document).on("touchstart", ".topic-item", function(event){
+            var o_event = event.originalEvent;
+            var touch = o_event.changedTouches[0];
+            _this.start_x = touch.pageX;
+            _this.cur_opt_item_id = $(event.currentTarget).attr("id");
+            console.log("++++ " + _this.cur_opt_item_id);
+        });
+
+        $(document).on("touchend", ".topic-item", function(event){
+            console.log(event);
+            var o_event = event.originalEvent;
+            var touch = o_event.changedTouches[0];
+            _this.end_x = touch.pageX;
+            var distance = Math.abs(_this.end_x - _this.start_x);
+            if(distance > 100) {
+                var cur = $(event.currentTarget);
+                
+                var id  = cur.attr("id");
+                var offset = cur.offset();
+                var height = cur.height();
+                var width  = cur.width();
+                console.log(offset);
+                $("#item-oper").offset({top:offset.top,left:offset.left});
+                console.log({top:offset.top,left:offset.left});
+                $("#item-oper").height(height);
+                $("#item-oper").width(width);
+                $("#item-oper").css("line-height", height + "px");
+                $("#item-oper").show();
+            }
+        });
+    },
+
+    _bind_oper_icon: function() {
+        var _this = this;
+        $(".item-oper_icon.edit").click(function(e){
+            $("#item-oper").hide();
+            console.log(_this);
+            var obj = _this._get_obj_by_id(_this.cur_opt_item_id);
+            console.log(obj);
+            _this.ssp.go("topic-edit");
+            $(".weui-textarea").val(obj.text);
+            _this._show_nav_bar_icon("topic-save");
+            _this.save_tag = 2;
+        });
+        $(".item-oper_icon.del").click(function(e){
+            $("#item-oper").hide();
+            console.log("******" + _this.cur_opt_item_id);
+            _this._remove_obj_by_id(_this.cur_opt_item_id);
+            _this.ssp.go("topic-list", _this._get_page_deep(), _this._get_cur_obj());
+        });
+        $(".item-oper_icon.return").click(function(e){
+            $("#item-oper").hide();
+        });
+    },
+
+    _show_nav_bar_icon: function(icon) {
+        if(icon == "topic-edit")
+        {
+            $(".topic-save").hide();
+            $(".topic-edit").show();
+        }
+        if(icon == "topic-save")
+        {
+            $(".topic-edit").hide();
+            $(".topic-save").show();
+        }
+    },
+
+    _set_cur_obj: function (obj) {
+        this.cur_object_ptr = obj;
+    },
+
+    _get_cur_obj: function () {
+        return this.cur_object_ptr;
+    },
+
+    _incr_page_deep: function () {
+        this.cur_object_deep++;
+    },
+
+    _set_page_deep: function (deep) {
+        this.cur_object_deep = deep;
+    },
+
+    _get_page_deep: function () {
+        return this.cur_object_deep;
+    },
+
+    _get_new_id: function () {
+        var d = new Date();
+        return "" + d.getTime();
+    },
+
+    _get_obj_by_id: function (id) {
+        var lst = this._get_cur_obj();
+        var obj = null;
+        for (var i = 0; i < lst.length; i++) {
+            obj = lst[i];
+            if(obj.id == id) {
+                break;
+            }
+        }
+        return obj;
+    },
+
+    _remove_obj_by_id: function(id) {
+        var lst = this._get_cur_obj();
+        console.log("\n--------------\n");
+        console.log(id);
+        console.log(lst);
+        var obj = null;
+        for (var i = 0; i < lst.length; i++) {
+            obj = lst[i];
+            if(obj.id == id) {
+                break;
+            }
+        }
+        if(i < lst.length) {
+            lst.splice(i, 1);
+        }
+    },
+}
